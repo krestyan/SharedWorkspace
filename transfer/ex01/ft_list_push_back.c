1f3f3b9366b07d5aaa5035d68b06a9cc76a72fda
Andrei KRESTYAN <akrestya@b1p31.academyplus.ro>
2017-10-25 19:19:32 +0300

    up

---
+++ transfer/ex01/ft_list_push_back.c
@@ -0,0 +1,20 @@
+#include "ft_list.h"
+
+t_list	*ft_create_elem(void *data);
+
+void	ft_list_push_back(t_list **begin_list, void *data)
+{
+	t_list	*crawl;
+
+	crawl = *begin_list;
+	if (*begin_list)
+	{
+		while (crawl->next != 0)
+			crawl = crawl->next;
+		crawl->next = ft_create_elem(data);
+	}
+	else
+	{
+		*begin_list = ft_create_elem(data);
+	}
+}
