ed4f9a4f1fa734678dff590e651a010bb49fbe99
Andrei KRESTYAN <akrestya@b1p31.academyplus.ro>
2017-10-25 18:59:36 +0300

    up

--- transfer/ex03/ft_list_size.c
+++ transfer/ex03/ft_list_size.c
@@ -1,14 +1,14 @@
 #include "ft_list.h"
 
-int     ft_list_size(t_list *begin_list)
+int		ft_list_size(t_list *begin_list)
 {
-    int count;
+	int count;
 
-    count = 0;
-    while(begin_list)
-    {
-        count++;
-        begin_list = begin_list->next;
-    }
-    return (count);
+	count = 0;
+	while (begin_list)
+	{
+		count++;
+		begin_list = begin_list->next;
+	}
+	return (count);
 }
