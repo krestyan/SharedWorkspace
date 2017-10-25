f5b46b5e75bf6613ebf6ca2287e6bb6e5a61988f
Andrei KRESTYAN <akrestya@b1p31.academyplus.ro>
2017-10-25 20:07:34 +0300

    up

--- transfer/ex07/ft_list_at.c
+++ transfer/ex07/ft_list_at.c
@@ -1,15 +1,14 @@
 #include "ft_list.h"
 
-t_list  *ft_list_at(t_list  *begin_list, unsigned int nbr)
+t_list	*ft_list_at(t_list *begin_list, unsigned int nbr)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while(begin_list && i < nbr)
-    {
-        i++;
-        begin_list = begin_list->next;
-    }
-
-    return(begin_list);
+	i = 0;
+	while (begin_list && i < nbr)
+	{
+		i++;
+		begin_list = begin_list->next;
+	}
+	return (begin_list);
 }
